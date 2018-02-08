fc21b163e3ce201831bd7b68fb51064a64b06cfe
avinriyan <avinriyan@yahoo.co.id>
2018-02-08 10:07:33 +0700

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simpleCRUDjs
+![Gambar Web](https://1.bp.blogspot.com/-XPeggDk2604/WnbP2s3etSI/AAAAAAAAAqQ/ZcPbzxmVqMkPJijTsW4wevvUCEslH2dYACLcBGAs/s1600/121.jpg)
+[Try demo](http://larapin.id/simplecrudjs/)
+
+[Tutorial Javascript oleh petanikode.com](https://www.petanikode.com/javascript-fungsi/) 
--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 var data = [
-    "Buku Tulis",
-    "Pensil",
-    "Spidol"
+    "Rekayasa Perangkat Lunak",
+    "Teknik Penyempurnaan Tekstil",
+    "Teknologi Pengolahan Hasil Pertanian",
+    "Teknik Konstruksi Batu dan Beton"
 ];
 
 
-function tampil(){
+function tampil() {
     var tabel = document.getElementById("tabel");
-    // clear list barang
     tabel.innerHTML = "<tr><th>No</th><th>Nama</th><th>Action</th></tr>";
-
-    // cetak semua barang
-    for(let i = 0; i < data.length; i++){
-        var btnEdit = "<button class='btn-edit' href='#' onclick='edit("+i+")'>Edit</button>";
-        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus("+i+")'>Hapus</button>";
-        j=i+1;
-        tabel.innerHTML += "<tr><td>"+j+"</td><td>" + data[i] + "</td><td>"+btnEdit +" "+ btnHapus +"</tr>";
+    for (let i = 0; i < data.length; i++) {
+        var btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Hapus</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
     }
 }
 
-function tambah(){
+function tambah() {
     var input = document.querySelector("input[name=barang]");
     data.push(input.value);
     tampil();
 }
 
-function edit(id){
+function edit(id) {
     var baru = prompt("Edit", data[id]);
     data[id] = baru;
     tampil();
 }
 
-function hapus(id){
+function hapus(id) {
     data.pop(id);
     tampil();
 }
